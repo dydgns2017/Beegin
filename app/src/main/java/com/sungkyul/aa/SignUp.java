af1857eaa5ef8331cbf847273c4005e4c58ce1e6
정용훈\정용훈 <asdf2017@naver.com>
2020-06-17 02:51:42 +0900

    modify_signup___ 기능 추가 : postApi

--- app/src/main/java/com/sungkyul/aa/SignUp.java
+++ app/src/main/java/com/sungkyul/aa/SignUp.java
@@ -1,26 +1,134 @@
 package com.sungkyul.aa;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+
+import com.sungkyul.aa.httpServlet.phpApi;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import static android.webkit.ConsoleMessage.MessageLevel.LOG;
 
 public class SignUp extends Activity {
+    EditText edtFullName, edtUserName, edtEmail, password, password_again;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_up);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
+        //  set ProgressDialog..
         Button btnSubmit = (Button)findViewById(R.id.btnSubmit);
+        edtFullName = (EditText)findViewById(R.id.edtFullName);
+        edtUserName = (EditText)findViewById(R.id.edtUserName);
+        edtEmail = (EditText)findViewById(R.id.edtEmail);
+        password = (EditText)findViewById(R.id.password);
+        password_again = (EditText)findViewById(R.id.password_again);
+
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                // Progressbar start
+                ProgressDialog progressDialog;
+                progressDialog = ProgressDialog.show(SignUp.this, "", "Loading..");
+
+                // data set
+                String serviceName = "signup";
+                String Servicedata;
+                Servicedata = "func=" + serviceName;
+                // editText data set
+                if ( edtFullName.getText().toString().equals("") || edtUserName.getText().toString().equals("") ||
+                        edtEmail.getText().toString().equals("") || password.getText().toString().equals("") || password_again.getText().toString().equals("")){
+                    progressDialog.dismiss();
+                    AlertDialog alertDialog = new AlertDialog.Builder(SignUp.this).create();
+                    alertDialog.setTitle("Error");
+                    alertDialog.setMessage("모든 정보를 입력해주세요.");
+                    alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            });
+                    alertDialog.show();
+                    return;
+                }
+                Servicedata += "&fullname=" + edtFullName.getText().toString();
+                Servicedata += "&username=" + edtUserName.getText().toString();
+                Servicedata += "&email=" + edtEmail.getText().toString();
+                Servicedata += "&password=" + password.getText().toString();
+                Servicedata += "&passagain=" + password_again.getText().toString();
+
+                // return json data
+                JSONObject data = phpApi.POSTsend(Servicedata);
+                System.err.println("Data : " + data);
+
+                // prev process
+                try {
+                    if ( data.toString().equals("") ||  data.getString("status") == "error" ){
+                        progressDialog.dismiss();
+                       return;
+                    }
+                    System.err.println("datagetString : " + data.getString("data"));
+                    if ( data.getString("data").toString().equals("password is wrong") ){
+                        progressDialog.dismiss();
+                        AlertDialog alertDialog = new AlertDialog.Builder(SignUp.this).create();
+                        alertDialog.setTitle("Error");
+                        alertDialog.setMessage("패스워드를 다시 입력해주세요.");
+                        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.dismiss();
+                                    }
+                                });
+                        alertDialog.show();
+                        System.err.println("패스워드를 다시 입력해주세요.");
+                        return;
+                    }
+                    if ( data.getString("data").toString().equals("existsUser") ){
+                        progressDialog.dismiss();
+                        AlertDialog alertDialog = new AlertDialog.Builder(SignUp.this).create();
+                        alertDialog.setTitle("Error");
+                        alertDialog.setMessage("이미 가입된 회원입니다.");
+                        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.dismiss();
+                                    }
+                                });
+                        alertDialog.show();
+                        System.err.println("가입된회원.");
+                        return;
+                    }
+                    // 아이디 중복 막기
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                // center process
+                System.err.println("아이디 생성완료");
+
+                // Progressbar exit
+                progressDialog.dismiss();
+
+                // LoginActivity 로 이동
                 Intent intent = new Intent(getApplicationContext(), loginActivity.class);
                 startActivity(intent);
+
+                // 현재 엑티비티 종료
                 finish();
             }
         });
--- app/src/main/java/com/sungkyul/aa/httpServlet/phpApi.java
+++ app/src/main/java/com/sungkyul/aa/httpServlet/phpApi.java
@@ -1,55 +1,90 @@
 package com.sungkyul.aa.httpServlet;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.media.tv.TvContract;
+import android.os.AsyncTask;
 import android.util.Log;
+import android.widget.ProgressBar;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.ArrayList;
 
+import static java.lang.Thread.sleep;
+
+
+// how to use it?
+// first u have to import this java file and then u type like example..
+/*
+    example code
+    ```
+    phpApi.setAddr();
+    String serviceName = "typeServiceName";
+    data = "func=" + serviceName;
+    data += "&otherData=otherData";
+    phpApi.sendPost(data);
+    ```
+ */
 public class phpApi {
     // https://androidexample.com/How_To_Make_HTTP_POST_Request_To_Server_-_Android_Example/index.php?view=article_discription&aid=64&aaid=89 --> EXAMPLE CODE
+    static String urlData = "http://34.64.177.188:8080/design_pattern.php";
     static URL url;
-    static JSONObject jsonObject;
-    static HttpURLConnection conn;
-    public static void setAddr(String urlData, String data){
+    static {
         try {
-            // URL setting
             url = new URL(urlData);
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
     }
+    static JSONObject returnJsonData;
 
-    public static void setData(JSONObject jsonArg){
-        jsonObject = jsonArg;
-    }
-
-    public static void sendPost(){
+    public static JSONObject POSTsend(final String data){
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Log.i("Start HTTP POST METHOD", "OK");
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setRequestMethod("POST");
+                    conn.setDoOutput(true);
+                    conn.connect();
+                    OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+                    wr.write(data);
+                    wr.flush();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                    returnJsonData = new JSONObject(reader.readLine().toString());
+                    // 연결 끊기
+                    wr.close();
+                    conn.disconnect();
+                    Log.i("Start HTTP POST METHOD", "END");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException err) {
+                    err.printStackTrace();
+                }
+            }
+        });
+        // start thread
+        thread.start();
         try {
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("POST");
-            conn.setRequestProperty("Content-Type", "application/json");
-            conn.setRequestProperty("Accept", "application/json");
-            conn.setDoOutput(true);
-            conn.setDoInput(true);
-            conn.connect();
-
-            DataOutputStream os = new DataOutputStream(conn.getOutputStream());
-            os.writeBytes(URLEncoder.encode(jsonObject.toString(), "UTF-8"));
-            os.flush();
-            os.close();
-
-            Log.i("STATUS", String.valueOf(conn.getResponseCode()));
-            Log.i("MSG", conn.getResponseMessage());
-
-            conn.disconnect();
-        } catch (IOException e) {
+            // wating thread
+            thread.join();
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        return returnJsonData;
     }
 }
