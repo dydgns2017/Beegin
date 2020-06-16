edc464b116a464e6210b1bb1057aa5a28bf320e5
Yonghoon Jung <asdf2017@naver.com>
2020-06-16 17:59:56 +0000

    php signup 기능추가

--- design_pattern.php
+++ design_pattern.php
@@ -12,12 +12,11 @@ function outputJSON($msg, $status = 'error'){
 }
 
 // $trace=yes;
-$trace=0;
+$trace=false;
 function s00_log($msg) {
     global $trace;
     if ($trace) error_log($msg);
 } 
-
 /////////////////////////////////////
 // null service
 $services['test'] = '_test';
@@ -26,16 +25,63 @@ function _test() {
     throw new Exception ( __FILE__.'is Available.');
 };
 
+// dir config
+$json_data_dir = "./json_data";
 /////////////////////////////////////
 // login service
 $services["login"] = "_login";
 function _login(){
+    global $json_data_dir;
     s00_log ("Start ".__FUNCTION__);
     $id = $_POST["id"];
     $pw = $_POST["pw"];
     outputJSON(array("id"=>$id, "pw"=>$pw), "success");
 }
+/////////////////////////////////////
+// signup Service
+$services["signup"] = "_signup";
+function _signup(){
+    global $json_data_dir;
+    $fullname = $_POST["fullname"];
+    $username = $_POST["username"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    $passagain = $_POST["passagain"];
+    error_log("$fullname : $username : $email : $password : $passagain ");
+    if ( $password != $passagain ) outputJSON("password is wrong");
+    
+    // prev task
+    // JSON 파일이 하나도 없을 경우
+    if ( !(file_exists("$json_data_dir/loginData.json")) ){
+        $array_data = array("all_count"=>1);
+        array_push($array_data, array("fullname"=>$fullname, 
+                                       "username"=>$username, 
+                                       "email"=>$email, 
+                                       "password"=>$password, 
+                                       "passagain"=>$passagain));
+    }
 
+    // JSON 파일이 있는 경우
+    if ( file_exists("$json_data_dir/loginData.json") ){
+        $array_data = json_decode(file_get_contents("$json_data_dir/loginData.json"), true);
+        // 계정 중복 확인
+        $count = $array_data["all_count"];
+        for ($i=0; $i < $count; $i++) { 
+            # code...
+            if ( $array_data[$i]["username"] == $username ) outputJSON("existsUser");
+        }
+        error_log("is it start?");
+        $array_data["all_count"] = $array_data["all_count"]+1;
+        array_push($array_data, array("fullname"=>$fullname, 
+                                       "username"=>$username, 
+                                       "email"=>$email, 
+                                       "password"=>$password, 
+                                       "passagain"=>$passagain));
+    }
+    // JSON 저장
+    file_put_contents("$json_data_dir/loginData.json", json_encode($array_data));
+    outputJSON("ok", "success");
+}
 
 $func = isset($_POST['func'])?$_POST["func"]:"test";
 
