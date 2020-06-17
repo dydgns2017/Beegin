18d24157ffe9fb391c84d7f3d93c892ae500e2a7
Hyuck Ju <skygurwn96@naver.com>
2020-06-17 18:10:11 +0900

    d

--- app/src/main/java/com/sungkyul/aa/AddWorkActivity.java
+++ app/src/main/java/com/sungkyul/aa/AddWorkActivity.java
@@ -2,6 +2,9 @@ package com.sungkyul.aa;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -10,12 +13,15 @@ import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -68,10 +74,14 @@ public class AddWorkActivity extends Activity {
 
     private void showAlertDialog() {
         // Prepare grid view
+        DialogInterface dia = null;
         GridView gridView = new GridView(this);
         ArrayList<Bitmap> picArr = new ArrayList<Bitmap>();
+        // Set grid view to alertDialog
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        final AlertDialog ad = builder.create();
 
-        Integer[] imgResource = {R.drawable.mov01, R.drawable.mov01, R.drawable.mov01, R.drawable.mov01, R.drawable.mov01, R.drawable.mov01, R.drawable.mov01, R.drawable.mov01, R.drawable.mov01, R.drawable.mov01,
+        final int[] imgResource = {R.drawable.mov01, R.drawable.mov01, R.drawable.mov01, R.drawable.mov01, R.drawable.mov01, R.drawable.mov01, R.drawable.mov01, R.drawable.mov01, R.drawable.mov01, R.drawable.mov01,
                 R.drawable.mov01, R.drawable.mov01, R.drawable.mov01, R.drawable.mov01, R.drawable.mov01, R.drawable.mov01, R.drawable.mov01, R.drawable.mov01, R.drawable.mov01, R.drawable.mov01,
                 R.drawable.mov01, R.drawable.mov01, R.drawable.mov01, R.drawable.mov01, R.drawable.mov01, R.drawable.mov01, R.drawable.mov01, R.drawable.mov01, R.drawable.mov01, R.drawable.mov01,
                 R.drawable.mov01, R.drawable.mov01, R.drawable.mov01, R.drawable.mov01, R.drawable.mov01, R.drawable.mov01, R.drawable.mov01, R.drawable.mov01, R.drawable.mov01, R.drawable.mov02, };
@@ -80,22 +90,87 @@ public class AddWorkActivity extends Activity {
             picArr.add(BitmapFactory.decodeResource(getResources(), imgResource[i]));
         }
 
-
-        gridView.setAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1, picArr));
-        gridView.setNumColumns(5);
+        ImageGridAdapter imageGridAdapter = new ImageGridAdapter(this, imgResource);
+        gridView.setAdapter(imageGridAdapter);
+        gridView.setNumColumns(4);
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 // do something here
+                Toast.makeText(getApplicationContext(), "test" + position, Toast.LENGTH_LONG).show();
+                btnaddImg.setBackgroundResource(imgResource[position]);
+                ad.dismiss();
             }
         });
 
-        // Set grid view to alertDialog
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setView(gridView);
-        builder.setTitle("이미지 선택");
-        builder.show();
+
+        ad.setView(gridView);
+        ad.setTitle("이미지 선택");
+        gridView.setPadding(20, 20, 20, 20);
+        ad.show();
+
     }
+
+
+    public class ImageGridAdapter extends BaseAdapter {
+
+        Context context = null;
+
+        //-----------------------------------------------------------
+        // imageIDs는 이미지 파일들의 리소스 ID들을 담는 배열입니다.
+        // 이 배열의 원소들은 자식 뷰들인 ImageView 뷰들이 무엇을 보여주는지를
+        // 결정하는데 활용될 것입니다.
+
+        int[] imageIDs = null;
+
+        public ImageGridAdapter(Context context, int[] imageIDs) {
+            this.context = context;
+            this.imageIDs = imageIDs;
+        }
+        public int getCount() {
+            return (null != imageIDs) ? imageIDs.length : 0;
+        }
+        public Object getItem(int position) {
+            return (null != imageIDs) ? imageIDs[position] : 0;
+        }
+        public long getItemId(int position) {
+            return position;
+        }
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imageView = null;
+
+            if (null != convertView)
+                imageView = (ImageView)convertView;
+            else {
+                //---------------------------------------------------------------
+                // GridView 뷰를 구성할 ImageView 뷰의 비트맵을 정의합니다.
+                // 그리고 그것의 크기를 320*240으로 줄입니다.
+                // 크기를 줄이는 이유는 메모리 부족 문제를 막을 수 있기 때문입니다.
+
+                Bitmap bmp
+                        = BitmapFactory.decodeResource(context.getResources(), imageIDs[position]);
+                bmp = Bitmap.createScaledBitmap(bmp, 320, 240, false);
+
+                //---------------------------------------------------------------
+                // GridView 뷰를 구성할 ImageView 뷰들을 정의합니다.
+                // 뷰에 지정할 이미지는 앞에서 정의한 비트맵 객체입니다.
+
+                imageView = new ImageView(context);
+                imageView.setAdjustViewBounds(true);
+
+
+                imageView.setImageBitmap(bmp);
+
+
+
+
+            }
+
+            return imageView;
+        }
+
+    }
+
 
 
 }
