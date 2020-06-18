9861f1cc1b2b6595049b382e698e2049e90bde84
정용훈\정용훈 <asdf2017@naver.com>
2020-06-18 18:04:40 +0900

    loginActivity 수정

--- app/src/main/java/com/sungkyul/aa/loginActivity.java
+++ app/src/main/java/com/sungkyul/aa/loginActivity.java
@@ -22,44 +22,29 @@ public class loginActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
-
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
         btnLogin = (Button)findViewById(R.id.btnlogin);
-
-//        btnLogin.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//                EditText id = (EditText)findViewById(R.id.id);
-//                EditText pass = (EditText)findViewById(R.id.password);
-//
-//                    if("test".equals(id.getText().toString().trim()) && "test".equals(pass.getText().toString().trim())) {
-//
-//                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-//                        startActivity(intent);
-//                    }else{
-//                        Toast.makeText(getApplicationContext(), "아이디 비밀번호가 틀렸어? (id,pwd : test)",Toast.LENGTH_LONG).show();
-//                    }
-//
-//
-//            }
-//        });
-
-
-
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText id = (EditText)findViewById(R.id.id);
+                EditText pass = (EditText)findViewById(R.id.password);
+                    if("test".equals(id.getText().toString().trim()) && "test".equals(pass.getText().toString().trim())) {
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(intent);
+                    }else{
+                        Toast.makeText(getApplicationContext(), "아이디 비밀번호가 틀렸어? (id,pwd : test)",Toast.LENGTH_LONG).show();
+                    }
+            }
+        });
         btnSignUp = (Button)findViewById(R.id.btnSignup);
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(), "click_signUp", Toast.LENGTH_LONG).show();
-               Intent intent = new Intent(getApplicationContext(), SignUp.class);
+                Intent intent = new Intent(getApplicationContext(), SignUp.class);
                 startActivity(intent);
             }
         });
-
-
     }
-
-
 }
