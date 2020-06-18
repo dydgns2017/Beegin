60509f2d8cae898e4aa1b2cd34a503039e3cd4a1
Hyuck Ju <skygurwn96@naver.com>
2020-06-18 23:53:31 +0900

    plan

--- app/src/main/java/com/sungkyul/aa/Fragment/PlanFragment.java
+++ app/src/main/java/com/sungkyul/aa/Fragment/PlanFragment.java
@@ -1,15 +1,29 @@
 package com.sungkyul.aa.Fragment;
 
 
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.sungkyul.aa.R;
+import com.sungkyul.aa.Result.ResultItem;
+import com.sungkyul.aa.Result.ResultItemView;
+import com.sungkyul.aa.myDBHelper;
+import com.sungkyul.aa.planFragment.ListViewAdapter;
+import com.sungkyul.aa.planFragment.ListViewItem;
+
+import java.util.ArrayList;
 
 
 /**
@@ -17,6 +31,9 @@ import com.sungkyul.aa.R;
  */
 public class PlanFragment extends Fragment {
 
+
+
+    View view;
 
     public PlanFragment() {
         // Required empty public constructor
@@ -26,8 +43,29 @@ public class PlanFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_plan, container, false);
+        view = inflater.inflate(R.layout.fragment_plan, null);
+        ListView listview ;
+        ListViewAdapter adapter = new ListViewAdapter();
+
+        // 리스트뷰 참조 및 Adapter달기
+        listview = (ListView) view.findViewById(R.id.listview1);
+
+        // 첫 번째 아이템 추가.
+        adapter.addItem(ContextCompat.getDrawable(getActivity(), R.drawable.ic_launcher_background),
+                "Box", "Account Box Black 36dp") ;
+        // 두 번째 아이템 추가.
+        adapter.addItem(ContextCompat.getDrawable(getActivity(), R.drawable.ic_launcher_background),
+                "Circle", "Account Circle Black 36dp") ;
+        // 세 번째 아이템 추가.
+        adapter.addItem(ContextCompat.getDrawable(getActivity(), R.drawable.ic_launcher_background),
+                "Ind", "Assignment Ind Black 36dp") ;
+
+        listview.setAdapter(adapter);
+
+//        return inflater.inflate(R.layout.fragment_plan, container, false);
+        return view;
     }
+
+
 
 }
